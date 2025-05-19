505d11cda8b7d969d4e293f5617d00b346d4a94f
Vivi <linmo0830@gmail.com>
2025-05-20 00:20:05 +0800

    更改 wokwi.toml 裡 firmware.bin/.elf 路徑

--- src/smoke_detector.ino
+++ src/smoke_detector.ino
@@ -1,70 +1,53 @@
-// #define MQ2_PIN 34
-// #define LED_PIN 26
-// #define BUZZER_PIN 25
-#define LED_PIN 18
-#define BUZZER_PIN 19
+#include <TM1637Display.h>
+
+#define MQ2_PIN 34      // 模擬輸入
+#define LED_PIN 25
+#define BUZZER_PIN 26
+#define CLK 27          // TM1637 CLK
+#define DIO 14          // TM1637 DIO
+
+TM1637Display display(CLK, DIO);
+int alertCount = 0;
 
 void setup() {
+  pinMode(MQ2_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
+  display.setBrightness(5); // 亮度 0~7
+  display.showNumberDec(0); // 初始顯示 0
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  //digitalWrite(LED_PIN, LOW);
-  //delay(500);
+  int gasDetected = digitalRead(MQ2_PIN);
+
+  if (gasDetected == LOW) { // 有煙霧（有些模組輸出低電位代表有氣體）
+    alertCount++;
+    display.showNumberDec(alertCount);
+    airRaidAlarm();  // 呼叫空襲聲音警報
+    Serial.println("⚠️ 偵測到煙霧！");
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    // digitalWrite(BUZZER_PIN, LOW);
+  }
+
+  delay(1000);
 }
-// int smokeThreshold = 300;  // 你可以調整這個值
-// int smokeCount = 0;
 
-// int segmentPins[] = {13, 14, 15, 16, 17, 18, 19}; // a~g
+void airRaidAlarm() {
+  // 上升音調
+  for (int freq = 400; freq <= 1000; freq += 10) {
+    tone(BUZZER_PIN, freq);
+    digitalWrite(LED_PIN, HIGH);
+    delay(10);
+  }
 
-// // 七段顯示數字對應編碼（共陰極）
-// const byte digits[10][7] = {
-//   {1,1,1,1,1,1,0}, // 0
-//   {0,1,1,0,0,0,0}, // 1
-//   {1,1,0,1,1,0,1}, // 2
-//   {1,1,1,1,0,0,1}, // 3
-//   {0,1,1,0,0,1,1}, // 4
-//   {1,0,1,1,0,1,1}, // 5
-//   {1,0,1,1,1,1,1}, // 6
-//   {1,1,1,0,0,0,0}, // 7
-//   {1,1,1,1,1,1,1}, // 8
-//   {1,1,1,1,0,1,1}  // 9
-// };
+  // 下降音調
+  for (int freq = 1000; freq >= 400; freq -= 10) {
+    tone(BUZZER_PIN, freq);
+    digitalWrite(LED_PIN, LOW);
+    delay(10);
+  }
 
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(MQ2_PIN, INPUT);
-//   pinMode(LED_PIN, OUTPUT);
-//   pinMode(BUZZER_PIN, OUTPUT);
-//   for (int i = 0; i < 7; i++) {
-//     pinMode(segmentPins[i], OUTPUT);
-//   }
-// }
-
-// void loop() {
-//   int smokeValue = analogRead(MQ2_PIN);
-//   Serial.println(smokeValue);
-
-//   if (smokeValue > smokeThreshold) {
-//     digitalWrite(LED_PIN, HIGH);
-//     digitalWrite(BUZZER_PIN, HIGH);
-//     smokeCount++;
-//     displayDigit(smokeCount % 10);
-//     delay(1000);
-//   } else {
-//     digitalWrite(LED_PIN, LOW);
-//     digitalWrite(BUZZER_PIN, LOW);
-//     displayDigit(smokeCount % 10);
-//   }
-
-//   delay(500);
-// }
-
-// void displayDigit(int num) {
-//   for (int i = 0; i < 7; i++) {
-//     digitalWrite(segmentPins[i], digits[num][i]);
-//   }
-// }
+  noTone(BUZZER_PIN); // 停止蜂鳴器
+}
